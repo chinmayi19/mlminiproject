7d8b0e5ce42a043c5ddd34119a402f91f042c191
chinmayi19 <chinmayikeerthirajgowda@gmail.com>
2026-04-09 08:02:19 +0530

    added to permit the request of the frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import predict
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Backend is working"}
+# ✅ ADD THIS (VERY IMPORTANT)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # allows frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+# Include routes
 app.include_router(predict.router)
